706365921067b913bea27a4c19be4d98c85f143c
Elag1988 <acevedoedgard1988@msn.com>
2023-05-15 20:28:57 -0500

    Actualizando el proyecto con los cambios que se hiceron el sabado 13 de mayo de 2023 donde se realizo la estructura basuca de la API

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 //Fichero principal del proyecto
+//guarda en la constante express la libreria express
+const express = require("express");
+
+
+//Se guardan en la constante app todos los metodos de la libreria express
+const app = express();
+
+//Creacion de la constante puerto
+const port = 3000;
+
+//creacion de la constante userDB y productDB
+
+const userDB = '/api/users';
+const productDB = '/api/products';
+
+//creacion de la constante userRoutes y productRoutes
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+
+app.use(express.json());
+
+//ruta para acceso a la infornacion de la base de datos
+
+app.use(userDB, userRoutes);
+app.use(productDB,productRoutes)
+
+app.listen(port, () => {console.log("El servidor se ejecuta en http://localhost:" + port)})
